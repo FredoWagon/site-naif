99228f306571fe1754b6752b447085be23ac5406
Frédéric Legrand <fedde.leg@gmail.com>
2020-04-22 19:16:28 +0200

    modal pas encore en place

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,18 @@
+
+    function readurl(input) {
+      console.log(input);
+            // if (input.files && input.files[0]) {
+            //     var reader = new FileReader();
+
+            //     reader.onload = function (e) {
+            //         $('#preview')
+            //             .attr('src', e.target.result);
+            //     };
+
+            //     reader.readAsDataURL(input.files[0]);
+            // }
+        }
+
+export { readurl };
+
+
