ea5f9752697bb5a4a9ce44a4321a4b71647c1595
Frédéric Legrand <fedde.leg@gmail.com>
2020-04-26 17:17:27 +0200

    ca le fait

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -4,29 +4,54 @@ class PaintingsController < ApplicationController
   end
 
   def show
+
+  end
+
+  def new
+    @painting = Painting.new
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def create
+    @painting = Painting.new(painting_params)
+
+    if @painting.save
+      redirect_to paintings_path, :notice => "Bien joué!"
+    else
+      redirect_to paintings_path, :notice => "merde!"
+    end
+
+
+  end
+
+  def update
+    @painting = Painting.find(params[:id])
+    @painting.update(painting_params)
+
+  end
+
+  def open_modal
     @painting = Painting.find(params[:id])
     respond_to do |format|
-    format.js
+      format.js
     end
-end
-
-def update
-  @painting = Painting.find(params[:id])
-  @painting.update(painting_params)
-  redirect_to paintings_path
-
-end
-
-def open_modal
-  @painting = Painting.find(params[:id])
-  respond_to do |format|
-    format.js
   end
-end
 
-private
+  def destroy
+    @painting = Painting.find(params[:id])
+    @painting.destroy
+      respond_to do |format|
+        format.js
+    end
 
-def painting_params
-  params.require(:painting).permit(:photo)
-end
+  end
+
+
+  private
+
+  def painting_params
+    params.require(:painting).permit(:photo, :name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :paintings, only: [:index, :show, :update] do
+  resources :paintings, only: [:index, :new, :show, :update, :create, :destroy] do
     get :open_modal, on: :member
   end
   resources :illustrations, only: [:index]
