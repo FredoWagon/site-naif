e115ec90c65c31495597dfa7f3e0715f6ca01a7c
Frédéric Legrand <fedde.leg@gmail.com>
2020-06-29 18:40:51 +0200

    deploy

--- app/javascript/components/navigation.js
+++ app/javascript/components/navigation.js
@@ -2,20 +2,26 @@ $(document).ready(function(){
   // menu click event
   $('.menuBtn').click(function() {
     $(this).toggleClass('act');
-      if($(this).hasClass('act')) {
-        $('.mainMenu').addClass('act');
-      }
-      else {
-        $('.mainMenu').removeClass('act');
-      }
+    if($(this).hasClass('act')) {
+      $('.mainMenu').addClass('act');
+    }
+    else {
+      $('.mainMenu').removeClass('act');
+    }
   });
 });
+
+
+
+
+
 
 
 document.addEventListener("turbolinks:load", function() {
   console.log("turbolinks");
   const menubb = document.querySelector('.menuBtn');
   menubb.classList.remove("act");
-});
+
+  });
 
 
